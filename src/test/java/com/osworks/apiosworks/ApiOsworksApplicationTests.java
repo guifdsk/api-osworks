7f9c7869e7d7931570ea4e6b7e0ddbb76d84dfa0
GuiFidelis <68570304+GuiFidelis@users.noreply.github.com>
2021-03-09 22:08:44 -0300

    Implementando uma REST API com Spring

---
+++ src/test/java/com/osworks/apiosworks/ApiOsworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osworks.apiosworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOsworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
